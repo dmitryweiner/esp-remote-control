59144591024699250aec88d3d9c566e776ffbed3
dmitryweiner <dmitry.weiner@gmail.com>
2018-02-13 22:02:10 +0700

    captive portal

--- src/server/server.ino
+++ src/server/server.ino
@@ -70,9 +70,24 @@ void loop() {
   server.handleClient();
 }
 
+/** Redirect to captive portal if we got a request for another domain. Return true in that case so the page handler do not try to handle the request again. */
+boolean captivePortal() {
+  if (!isIp(server.hostHeader()) && server.hostHeader() != (String(myHostname)+".local")) {
+    Serial.print("Request redirected to captive portal");
+    server.sendHeader("Location", String("http://") + toStringIp(server.client().localIP()), true);
+    server.send ( 302, "text/plain", ""); // Empty content inhibits Content-length header so we have to close the socket ourselves.
+    server.client().stop(); // Stop is needed because we sent no content length
+    return true;
+  }
+  return false;
+}
+
 /** Handle root or redirect to captive portal */
 void handleRoot() {
-  String response = "INDEX";
+  if (captivePortal()) { // If caprive portal redirect instead of displaying the page.
+    return;
+  }
+  String response = "INDEX"; // this string will be replaced
   server.sendHeader("Cache-Control", "no-cache, no-store, must-revalidate");
   server.sendHeader("Pragma", "no-cache");
   server.sendHeader("Expires", "-1");
@@ -106,6 +121,9 @@ void handleControl() {
 }
 
 void handleNotFound() {
+  if (captivePortal()) { // If caprive portal redirect instead of displaying the page.
+    return;
+  }
   String message = "File Not Found\n\n";
   message += "URI: ";
   message += server.uri();
@@ -125,4 +143,23 @@ void handleNotFound() {
   Serial.println("404 page served");
 }
 
+/** Is this an IP? */
+boolean isIp(String str) {
+  for (int i = 0; i < str.length(); i++) {
+    int c = str.charAt(i);
+    if (c != '.' && (c < '0' || c > '9')) {
+      return false;
+    }
+  }
+  return true;
+}
 
+/** IP to String? */
+String toStringIp(IPAddress ip) {
+  String res = "";
+  for (int i = 0; i < 3; i++) {
+    res += String((ip >> (8 * i)) & 0xFF) + ".";
+  }
+  res += String(((ip >> 8 * 3)) & 0xFF);
+  return res;
+}
